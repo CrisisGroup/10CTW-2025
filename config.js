a8deb56584c934c52f4618cef7095ed83dcd0e66
Paul Franz <paulkfranz@gmail.com>
2025-01-07 16:10:15 -0500

    style refresh v2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-  style: "mapbox://styles/daltonwb/cm5my3vk200cv01s366kf0ndn",
+  style: "mapbox://styles/daltonwb/cm5mypjso00c901pl9teo5cvc",
   accessToken:
     "pk.eyJ1IjoiZGFsdG9ud2IiLCJhIjoiOWdSSXFQSSJ9.HZyjh4g3TAAOAncwelv9Vw",
   showMarkers: false,
