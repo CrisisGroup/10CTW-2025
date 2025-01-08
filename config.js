c8b0d2789288b80f571814b1c515c316c98b5836
Paul Franz <paulkfranz@gmail.com>
2025-01-08 06:14:57 -0500

    photo caption fixes, legend fix

--- config.js
+++ config.js
@@ -72,7 +72,7 @@ var config = {
       title: "Syria",
       image: "",
       description:
-        "The Assad regime is gone after 54 years in power. Syria has a chance to get back on its feet. But after years of bloody civil war, plenty could go wrong. For Syria’s new authorities, preventing disorder and allowing for an inclusive transition are priorities. Foreign powers should loosen sanctions that obstruct aid and investment." + '<div class="spacer"></div>' + '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#syria" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/syria.jpg" /><p class="photocaption">A man rides a motorbike with children holding up flags adopted by the new Syrian rulers, after Syria’s Bashar al-Assad was ousted, in Damascus, Syria, December 15, 2024. REUTERS / Ammar Awad</p>',
+        "The Assad regime is gone after 54 years in power. Syria has a chance to get back on its feet. But after years of bloody civil war, plenty could go wrong. For Syria’s new authorities, preventing disorder and allowing for an inclusive transition are priorities. Foreign powers should loosen sanctions that obstruct aid and investment." + '<div class="spacer"></div>' + '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#syria" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/syria.jpg" /><p class="photocaption">A man rides a motorbike with children holding up flags adopted by the new Syrian rulers, after Syria’s Bashar al-Assad was ousted, in Damascus, Syria. December 2024. REUTERS / Ammar Awad</p>',
       location: {
         center: [38.41918, 35.04736],
         zoom: 6.5,
@@ -245,7 +245,7 @@ var config = {
       description:
         "Trump says he’ll end the Ukraine war through negotiations with Russian President Vladimir Putin. Talks are worth trying but it is hard to see a path to a sustainable deal. Meanwhile, Putin may be tempted to test Trump’s commitment to European allies – creating new risks of confrontation and escalation." +
         '<div class="spacer"></div>' +
-        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#ukraine" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/ukraine.jpg" /><p class="photocaption">Ukrainian Armed Forces take part in a tactical medicine exercise, amid Russia’s attack on Ukraine, in Donetsk region, Ukraine December 14, 2024. Oleg Petrasiuk / Press Service of the 24th King Danylo Separate Mechanized Brigade of the Ukrainian Armed Forces / Handout via REUTERS</p>',
+        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#ukraine" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/ukraine.jpg" /><p class="photocaption">Ukrainian Armed Forces take part in a tactical medicine exercise, amid Russia’s attack on Ukraine, in Donetsk region, Ukraine. December 2024. Oleg Petrasiuk / Press Service of the 24th King Danylo Separate Mechanized Brigade of the Ukrainian Armed Forces / Handout via REUTERS</p>',
       location: {
         center: [30.99252, 51.17468],
         zoom: 5,
@@ -409,7 +409,7 @@ var config = {
       description:
         "Following Israel’s hammering of Hizbollah in Lebanon, and the Assad regime’s fall in Syria, Iran looks more vulnerable than at any time in decades. While U.S. and Israeli hawks push for strikes on Iranian nuclear facilities or even regime change, talks to limit Tehran’s nuclear program and support for proxies offer a better way forward." +
         '<div class="spacer"></div>' +
-        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#iran" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/iran.jpg" /><p class="photocaption">Iranian President Masoud Pezeshkian and military commanders watch as military equipment passes by during the annual military parade in Tehran, Iran, September 21, 2024. Majid Asgaripour / WANA (West Asia News Agency) via REUTERS</p>',
+        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#iran" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/iran.jpg" /><p class="photocaption">Iranian President Masoud Pezeshkian and military commanders watch as military equipment passes by during the annual military parade in Tehran, Iran. September 2024. Majid Asgaripour / WANA (West Asia News Agency) via REUTERS</p>',
       location: {
         center: [-72.277873, 18.57867],
         zoom: 16.5,
@@ -486,7 +486,7 @@ var config = {
       title: "Haiti",
       image: "",
       description:
-        "Many Haitians hoped a new government and a Kenya-led security mission could rein in the gangs wreaking havoc in their country. Those hopes have now faded. Haiti’s government is calling for UN peacekeepers though whether such a mission is feasible or would be effective is unclear." + '<div class="spacer"></div>' + '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#haiti" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/haiti.jpg" /><p class="photocaption">Members of the second contingent of Kenyan police disembark after arriving in the Caribbean country as part of a peacekeeping mission, in Port-au-Prince, Haiti 16 July, 2024. REUTERS / Ralph Tedy Erol</p>',
+        "Many Haitians hoped a new government and a Kenya-led security mission could rein in the gangs wreaking havoc in their country. Those hopes have now faded. Haiti’s government is calling for UN peacekeepers though whether such a mission is feasible or would be effective is unclear." + '<div class="spacer"></div>' + '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#haiti" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/haiti.jpg" /><p class="photocaption">Members of the second contingent of Kenyan police disembark after arriving in the Caribbean country as part of a peacekeeping mission, in Port-au-Prince, Haiti. July 2024. REUTERS / Ralph Tedy Erol</p>',
       location: {
         center: [-73.33375, 18.98953],
         zoom: 7.5,
@@ -539,7 +539,7 @@ var config = {
       title: "U.S.-Mexico",
       image: "",
       description:
-        "During his campaign, Trump promised to slap high tariffs on Mexican goods and deport millions of migrants. Within his Republican party, some suggest the U.S. should unilaterally bomb fentanyl labs in Mexico. Those policies would tank relations between the two countries, whose economies and security depend on cooperation." + '<div class="spacer"></div>' + '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#mexico" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/mexico.jpg" /><p class="photocaption">Republican presidential candidate and former U.S. President Donald Trump speaks with Texas Governor Greg Abbott during a visit at the U.S.-Mexico border at Eagle Pass, Texas, as seen from Piedras Negras, Mexico, February 29, 2024. REUTERS / Go Nakamura</p>',
+        "During his campaign, Trump promised to slap high tariffs on Mexican goods and deport millions of migrants. Within his Republican party, some suggest the U.S. should unilaterally bomb fentanyl labs in Mexico. Those policies would tank relations between the two countries, whose economies and security depend on cooperation." + '<div class="spacer"></div>' + '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#mexico" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/mexico.jpg" /><p class="photocaption">Republican presidential candidate and former U.S. President Donald Trump speaks with Texas Governor Greg Abbott during a visit at the U.S.-Mexico border at Eagle Pass, Texas, as seen from Piedras Negras, Mexico. February 2024. REUTERS / Go Nakamura</p>',
       location: {
         center: [-107.26147, 28.07947],
         zoom: 4,
@@ -648,7 +648,7 @@ var config = {
       image: "",
       description:
         "2024 began with Kim Jong-un abandoning North Korea’s decades-old policy of peaceful unification with South Korea. It ended with Pyongyang ratifying a mutual defence pact with Moscow and President Yoon Suk-yeol attempting a coup in Seoul. Trump could make a new push on nuclear diplomacy, but negotiations would be tougher than during his first term. The peninsula is set for a difficult 2025." + '<div class="spacer"></div>' +
-        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#korea" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/korea.jpg" /><p class="photocaption">People watch a TV broadcasting a news report on North Korea firing missiles that flew 400 km after lifting off at around 7:30 a.m. from Sariwon, just south of the capital Pyongyang, at a railway station in Seoul, South Korea, November 5, 2024. REUTERS / Kim Hong-Ji</p>',
+        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#korea" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/korea.jpg" /><p class="photocaption">People watch a TV broadcasting a news report on North Korea firing missiles that flew 400km after lifting off at around 7:30am from Sariwon, just south of the capital Pyongyang, at a railway station in Seoul, South Korea. November 2024. REUTERS / Kim Hong-Ji</p>',
       location: {
         center: [126.09752, 37.75520],
         zoom: 5.5,
@@ -722,7 +722,7 @@ var config = {
       image: "",
       description:
         "Competition between the world’s two major powers continues to intensify. Tariffs, Taiwan and Chinese ambitions in the South China Sea are among the potential flashpoints. Trump’s scepticism toward alliances unsettles Asia almost as much as it does Europe." + '<div class="spacer"></div>' + 
-        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#china" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/china.jpg" /><p class="photocaption">Taiwanese navy Kuang Hua VI-class missile boats move within the harbour of Keelung, Taiwan October 14, 2024. REUTERS / Tyrone Siu</p>',
+        '<a href="https://www.crisisgroup.org/global/10-conflicts-watch-2025#china" target="_blank">&#8594; [Read more]</a>' + '<div class="spacer"></div>' + '<img src="images/china.jpg" /><p class="photocaption">Taiwanese navy Kuang Hua VI-class missile boats move within the harbour of Keelung, Taiwan. October 2024. REUTERS / Tyrone Siu</p>',
       location: {
         center: [113.41247, 13.11950],
         zoom: 4.5,
